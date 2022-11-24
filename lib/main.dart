98fecc139b20e0fefebf988c9df58489d3e8b738
Hoàng Phú Nam <hoangphu.nam0604@gmail.com>
2022-11-24 15:05:10 +0700

    infinite scrolling list

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup Name Generator'),
         ),
         body: const  Center(
           child: RandomWords(),
@@ -36,7 +36,34 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text("Random Word is: "+wordPair.asPascalCase);
+    final _suggesions = <WordPair>[];
+    final _biggerFont = const TextStyle(fontSize: 18);
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0), 
+      /**
+       * Lớp ListView cung cấp thuộc tính itemBuilder
+       * nó là factory builder và hàm callback là 1 hàm ẩn danh
+       * 
+       * itemBuilder callback được gọi một lần trên mỗi hàng
+       */
+      itemBuilder: (context, i){
+        // Đối với hàng lẽ, thêm đường phần cách
+        if(i.isOdd) return const Divider();
+
+      /**
+       * The expression i ~/ 2 divides i by 2 and returns an integer result. For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2. This calculates the actual number of word pairings in the ListView, minus the divider widgets.
+       */
+        final index = i ~/ 2;
+
+        if(index >= _suggesions.length){
+          _suggesions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggesions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
   }
 }
